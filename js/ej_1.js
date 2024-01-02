74bbe8727a7df7dbd83bf6e4fa4d221dc5515d78
Hernan Soria <hernansoria02@gmail.com>
2024-01-02 05:23:50 -0300

    se realizo el ejercicio 1

---
+++ js/ej_1.js
@@ -0,0 +1,8 @@
+let edad = parseInt(prompt('Ingresar edad'))
+ if(edad >= 18){
+    document.write('Tiene permiso para conducir')
+ }
+ else{
+    document.write('No tiene permiso para conducir')
+ }
+ 
