7658b5a3952d903cbfac72bd3b1565ce68fd4c06
Hernan Soria <hernansoria02@gmail.com>
2024-01-06 20:50:07 -0300

    se dio formato

--- js/ej6.js
+++ js/ej6.js
@@ -1,10 +1,10 @@
-let piramide = '';
+let piramide = "";
 for (let i = 1; i <= 30; i++) {
-  let fila = '';
+  let fila = "";
   for (let j = 1; j <= i; j++) {
-   fila = fila + i;
+    fila = fila + i;
   }
-  piramide = piramide + fila + '<br>';
+  piramide = piramide + fila + "<br>";
 }
 
 document.write(piramide);
