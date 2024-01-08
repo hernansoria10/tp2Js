d798ee22e8c5e00a018ac3f7e52f5097b3cb0425
Hernan Soria <hernansoria02@gmail.com>
2024-01-07 21:18:24 -0300

    se realizo ej 9

---
+++ js/ej9.js
@@ -0,0 +1,19 @@
+for (let i = 1; i <= 500; i++) {
+  if (i % 4 !== 0 && i % 9 !== 0) {
+    document.write(i + "<br>");
+  }
+  if (i % 4 === 0 || i % 9 === 0 ) {
+    if(i % 4 === 0 && i % 9 === 0){
+        document.write(i + " (es multiplo de 4 y 9)" +"<br>")
+    }
+    else if( i % 4 === 0){
+    document.write(i + " (multiplo de 4)" + "<br>");}
+    else if (i % 9 === 0) {
+        document.write(i + " (multiplo de 9)" + "<br>");
+      }
+  }
+  let linea = ' <span class = "fontyeah"> ------------------------------ </span> <br>';
+  if (i % 5 === 0) {
+    document.write(linea);
+  }
+}
