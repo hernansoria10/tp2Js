5e0d002899d28140bf6ebec49b21c093416cdf72
Hernan Soria <hernansoria02@gmail.com>
2024-01-11 21:10:19 -0300

    se hizo una modificacion

--- js/ej12.js
+++ js/ej12.js
@@ -1,6 +1,6 @@
 let numeroRandom = Math.ceil(Math.random() * 100)
 console.log(numeroRandom)
 if(numeroRandom == 100){
-    numeroRandom--
+    numeroRandom = numeroRandom - Math.ceil(Math.random() * 10)
 }
 document.write("<div>"+  numeroRandom+ "</div>"  )
