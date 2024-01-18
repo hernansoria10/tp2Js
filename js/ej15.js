a9fc0bf1a932d00291c9513a7d4f893bcaf7947e
Hernan Soria <hernansoria02@gmail.com>
2024-01-18 17:24:45 -0300

    se corrigieron pequeñas cosas

--- js/ej15.js
+++ js/ej15.js
@@ -1,14 +1,13 @@
 let cadena = prompt("ingrese una cadena");
 cadena = cadena.toUpperCase()
 const vocales = "AEIOU"
-let vocalesEncontradas = ""
 let contador = 0
 for ( let i = 0; i < cadena.length ; i++){
     
     if(vocales.includes(cadena.charAt(i))){
         contador = contador+1 
-        vocalesEncontradas = vocalesEncontradas + contador       
-    }
    
 }
+}
+
  document.write(` "${cadena}" tiene ${contador} vocales`)
